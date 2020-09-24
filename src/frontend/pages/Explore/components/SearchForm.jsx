bfa849eace5914140c42be2af22e64bea4cc3007
mattiacoppini <mattiacoppini@gmail.com>
2020-09-24 23:48:24 +0200

    explore form now uses html form, settings page has now an implemented callback

--- src/frontend/pages/Explore/components/SearchForm.jsx
+++ src/frontend/pages/Explore/components/SearchForm.jsx
@@ -6,12 +6,19 @@ import OutlinedButton from "../../../components/UI/Buttons/OutlinedButton";
 const SearchForm = ({ onSubmitCallback }) => {
 	const [searchText, setSearchText] = useState();
 	const onChange = useCallback((e) => setSearchText(e.target.value), [setSearchText]);
-	const onClick = useCallback(() => onSubmitCallback(searchText), [onSubmitCallback, searchText]);
+	const onSubmit = useCallback(
+		(e) => {
+			e.preventDefault();
+			onSubmitCallback(searchText);
+		},
+		[onSubmitCallback, searchText]
+	);
+
 	return (
-		<>
+		<form onSubmit={onSubmit}>
 			<OutlinedInput onChange={onChange} placeholder="Search for an addon..." />
-			<OutlinedButton onClick={onClick}>Search</OutlinedButton>
-		</>
+			<OutlinedButton type="submit">Search</OutlinedButton>
+		</form>
 	);
 };
 
--- src/frontend/pages/Settings/index.jsx
+++ src/frontend/pages/Settings/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import styled from "styled-components";
 import H1 from "../../components/UI/Heading";
 import Box from "../../components/UI/Layout/Box";
@@ -26,6 +26,14 @@ const TableStyles = styled(Box)`
 `;
 
 const Settings = () => {
+	// TODO: ALREADY SAVE VALUE SHOULD BE LOADED AS USESTATE ARG
+	const [wowPath, setWowPath] = useState();
+	const onChangeWowPath = useCallback((e) => setWowPath(e.target.value), [setWowPath]);
+	const onWowPathSave = useCallback(() => {
+		// TODO: INSERT HERE CALLBACK TO BACKEND WITH WOW PATH VALUE
+		console.log("Wow path value:", wowPath);
+	}, [wowPath]);
+
 	return (
 		<Box color={UIToken.secondaryColor} fullWidth fullHeight padding={[UIToken.spacingLg]}>
 			<H1>Settings</H1>
@@ -35,8 +43,8 @@ const Settings = () => {
 						<tr>
 							<td>WoW path</td>
 							<td>
-								<OutlinedInput />
-								<OutlinedButton>Edit</OutlinedButton>
+								<OutlinedInput value={wowPath} onChange={onChangeWowPath} />
+								<OutlinedButton onClick={onWowPathSave}>Save</OutlinedButton>
 							</td>
 						</tr>
 					</tbody>
